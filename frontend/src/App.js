5fd3cebb9fecb9e567bd08f7ca52335f67d14a16
pratm1304 <prathammaske1304@gmail.com>
2025-12-20 15:52:24 +0530

    better sound

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -64,10 +64,11 @@ function App() {
       gainNode.connect(audioContext.destination);
       oscillator.frequency.value = 800;
       oscillator.type = 'sine';
-      gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-      gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
-      oscillator.start(audioContext.currentTime);
-      oscillator.stop(audioContext.currentTime + 0.5);
+      gainNode.gain.setValueAtTime(0.6, audioContext.currentTime);
+gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 1.0);
+
+oscillator.start(audioContext.currentTime);
+oscillator.stop(audioContext.currentTime + 1.0);
     } catch (error) {
       console.log('Audio error:', error);
     }
